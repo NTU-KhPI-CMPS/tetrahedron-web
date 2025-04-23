c8637fe26ea997291f73d1950c8e2777e35ee069
Jalkhet <118116354+Max1mj@users.noreply.github.com>
2025-04-23 16:21:01 +0100

    Tet 287/Update design of left toolbar (#73)
    * changes before merge
    
    * updated design of left sidebar
    
    * translateY update
    
    * updated buttons styling
    
    * TET-287: add animation
    
    * updated sidebar
    
    * updated height of sidebar
    
    * updated version
    
    ---------
    
    Co-authored-by: Illia Pasichnichenko <sheredan2003@gmail.com>

---
+++ src/components/ui/LeftRightIcon.tsx
@@ -0,0 +1,19 @@
+const LeftRightIcon = () => {
+  return (
+    <svg
+      width="26"
+      height="26"
+      viewBox="0 0 26 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="stroke-current"
+    >
+      <path d="M8.66683 3.25L4.3335 7.58333L8.66683 11.9167" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M4.3335 7.58325L21.6668 7.58325" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M17.3335 22.7499L21.6668 18.4166L17.3335 14.0833" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M21.6668 18.4167L4.3335 18.4167" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  )
+}
+
+export default LeftRightIcon
