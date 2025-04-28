96a0280880bbc15315ca4c7d3bc49c6617353c04
Jalkhet <118116354+Max1mj@users.noreply.github.com>
2025-04-28 11:42:24 +0100

    updated style

--- src/components/SwitchWithTitle.tsx
+++ src/components/SwitchWithTitle.tsx
@@ -9,10 +9,10 @@ interface SwitchWithTitleProps extends React.ComponentPropsWithoutRef<typeof Swi
 
 const SwitchWithTitle = ({ label, labelStyles, disabled, ...props }: SwitchWithTitleProps) => {
   return (
-    <div className="flex items-center">
+    <div className="flex w-[202px] items-center">
       <label
         htmlFor={props.id}
-        className={cn('flex-1 cursor-pointer', labelStyles, {
+        className={cn('tetrahedron-text-sm w-full flex-1 cursor-pointer font-semibold leading-[16px]', labelStyles, {
           'cursor-not-allowed': disabled
         })}
       >
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:text-[#747474] disabled:bg-grey-disabled',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md tetrahedron-text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:text-[#747474] disabled:bg-grey-disabled',
   {
     variants: {
       variant: {
@@ -23,7 +23,8 @@ const buttonVariants = cva(
         wide: 'h-10 px-6 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10 hover:bg-button-pressed hover:text-soft'
+        icon: 'h-10 w-10 hover:bg-button-pressed hover:text-soft',
+        custom: 'w-full h-6 py-2 px-8 rounded-sm '
       }
     },
     defaultVariants: {
